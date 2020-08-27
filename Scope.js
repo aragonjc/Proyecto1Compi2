11cdd6075123d8ca2357aba723a62085caf30f4f
aragonjc <juank1316@gmail.com>
2020-08-27 16:21:38 -0600

    funciones en scope.js

---
+++ Scope.js
@@ -0,0 +1,51 @@
+class Scope {
+    constructor(prev) {
+        this.prev = prev;
+        this.table = new Map();
+        this.functionTable = new Map();
+    }
+
+    get existLocalVariable(id) {
+
+    }
+
+    set insertVariable(id,obj,byval) {
+
+    }
+
+    get searchVariable(id) {
+
+    }
+    
+    get getObjectVariable(id) {
+
+    }
+
+    get existVariable(id) {
+
+    }
+
+    get existVariableNoGlobal(id) {
+
+    }
+
+    set changeVariableValue(id, nodeValue, byval) {
+
+    }
+
+    set insertFunction(id,obj) {
+
+    }
+
+    get searchFunction(id) {
+
+    }
+
+    get existFunction(id) {
+        
+    }
+
+    get globalScope() {
+
+    }
+}
