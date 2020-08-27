572cbc455db15080625a992deaf9298b3b7a623c
aragonjc <juank1316@gmail.com>
2020-08-27 17:14:57 -0600

    modificar console.log

--- callFunction.js
+++ callFunction.js
@@ -1,4 +1,5 @@
 const Node = require('./Node.js');
+const TObject = require('./TObject.js');
 class callFunction extends Node{
     constructor(line, column,id,params) {
         super(line,column,id);
@@ -9,9 +10,25 @@ class callFunction extends Node{
         
         if(id == "console.log") {
             if(this.params != null) {
+                if(this.params.length == 1) {
+                    
+                    let tobj = this.params.pop();
+                    console.log("----------------------");
+                    console.log(tobj.value)
 
+                } else {
+                    new Error(this.params.line,
+                        this.params.column,
+                        "Semantico",
+                        "Error la funcion necesita un parametro");
+                    console.log("-------ERROR----------");
+                }
             } else {
-                //error
+                new Error(this.params.line,
+                        this.params.column,
+                        "Semantico",
+                        "Error la funcion necesita un parametro");
+                console.log("-------ERROR----------");
             }
         } else {
 
